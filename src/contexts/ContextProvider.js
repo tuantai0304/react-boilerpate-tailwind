33a05973e514d70fd53f5f6b453ab104bb119f9b
Tai <tuantai0304@gmail.com>
2022-08-03 08:08:34 +1000

    Init commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,15 @@
+import React, { useContext, createContext, useState } from  'react'
+
+
+const StateContext = createContext();
+
+export const ContextProvider = ( {children} ) => {
+
+    return (
+        <StateContext.Provider value={ {  } }>
+            {children}
+        </StateContext.Provider>
+    )
+};
+
+export const useStateContext = () => useContext(StateContext);
